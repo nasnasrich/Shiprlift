f4d431705e39bc11371553e8783e69e46f0b0f4f
nasnasrich <nasnasrich@gmail.com>
2026-03-14 14:38:40 -0700

    jh

--- src/components/ResponsiveAppBar.jsx
+++ src/components/ResponsiveAppBar.jsx
@@ -43,7 +43,7 @@ function ResponsiveAppBar() {
         paddingTop: 0,
         boxShadow:
           "2px 3px 1px rgba(30,30,31), inset 0 0 5px rgba(25,25,26,0.22)",
-        height: { xs: 40, md: 60}, // <-- Reduced height on mobile
+        height: { xs: 45, md: 60}, // <-- Reduced height on mobile
       }}
     >
       <Container maxWidth="xl">
